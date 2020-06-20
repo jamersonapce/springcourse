f90c0c12e20ace017c8a622bfde88ac4f5069fe3
apce <jamerson.silva@acad.ifma.edu.br>
2020-06-19 23:58:45 -0300

    database H2 created

--- src/main/java/com/apce/domain/CategoryDomain.java
+++ src/main/java/com/apce/domain/CategoryDomain.java
@@ -2,15 +2,26 @@ package com.apce.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
+@Entity
+@Table(name = "TB_CATEGORIA")
 @Builder
 @ToString(exclude = "id")
 @EqualsAndHashCode(of = "id")
 public class CategoryDomain implements Serializable{
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 }
